8c987eb21cde2dff40cad6d9ff264b211f896cad
Aliy Akhbar <aliyakhbar@gmail.com>
2023-03-22 20:16:28 +0800

    Feature/sanity preview (#2)
    * Added preview pages that has content
    
    * Update live preview
    
    * Update live preview

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,35 +1,94 @@
-import Layout from '@/components/layout'
-import Image from 'next/image';
-import { Projects } from '@root/typings'
-
-import { allProjects, projectsPostsQuery } from 'lib/sanity.queries'
-import client from '@root/lib/sanity.client';
-import { urlForImage } from '@root/lib/sanity.image';
 import PortableText from 'react-portable-text';
+import { PreviewSuspense } from 'next-sanity/preview'
+import { lazy } from 'react'
+import client from '@lib/sanity.client'
+import Layout from '@/components/layout'
+import { Projects, Preview } from '@root/typings'
+import { allProjects, projectsPostsQuery } from 'lib/sanity.queries'
 
-interface Props {
-	datas: Projects;
+import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { urlForImage } from '@root/lib/sanity.image';
+
+const inter = Inter({
+	subsets: ['latin'],
+	variable: '--font-inter',
+})
+
+const PreviewProjectsInnerPage = lazy(() => import('@components/Studio/PreviewProjectsInnerPage'))
+
+
+export const getStaticPaths = async () => {
+	const posts = await client.fetch(allProjects)
+
+	const path = posts.map((post: any) => ({
+		params: { slug: post.slug.current },
+	}))
+
+	return {
+		paths: path,
+		fallback: 'blocking',
+	}
 }
 
-const Project = ({ datas }: Props) => {
+export const getStaticProps = async ({ preview = false, params }: any) => {
+	if (preview) {
+		return { props: { preview } }
+	}
+
+	const post = await client.fetch(projectsPostsQuery, { slug: params?.slug })
+
+	if (!post) {
+		return {
+			notFound: true
+		}
+	}
+
+	return {
+		props: {
+			data: post
+		},
+
+		revalidate: 60 // after 60 seconds, it will be regenerated
+	}
+}
+
+// loading the preview component
+export const loading = () => (
+	<div className={`flex justify-center items-center h-screen w-screen ${inter.variable} font-sans`}>
+		<h1>Loading...</h1>
+	</div>
+)
+
+export default function IndexPage({ preview, data }: {
+	preview: Preview;
+	data: Projects;
+}) {
+	if (preview) {
+		return (
+			<PreviewSuspense fallback={loading()}>
+				<PreviewProjectsInnerPage />
+			</PreviewSuspense>
+		)
+	}
 
 	return (
-		<main key={datas._id}>
+		<main key={data._id}>
 			<Layout>
 				<article>
 					<div className="rounded-xl overflow-hidden flex flex-col gap-5">
 						<section className="min-h-[50vh] h-auto w-full bg-slate-50 flex justify-center flex-col gap-12 p-16 text-black rounded-2xl">
 							<div className="w-full xl:w-[50%]">
-								{datas.title && (
-									<h1 className="text-2xl md:text-3xl lg:text-4xl font-medium">{datas.title}</h1>
+								{data.title && (
+									<h1 className="text-2xl md:text-3xl lg:text-4xl font-medium">{data.title}</h1>
 								)}
 
-								{datas.mainImage && (
+								{data.mainImage && (
 									<div className="relative w-full h-80 drop-shadow-xl">
 										<Image
 											className='object-cover object-left lg:object-center py-4'
-											src={urlForImage(datas.mainImage).url()}
-											alt={datas.mainImage.alt ? datas.mainImage.alt.current : datas.title}
+											src={urlForImage(data.mainImage).url()}
+											alt={data.mainImage.alt ? data.mainImage.alt.current : data.title}
 											fill
 										/>
 									</div>
@@ -37,27 +96,27 @@ const Project = ({ datas }: Props) => {
 							</div>
 
 							<div className="flex flex-col md:flex-row gap-10">
-								{datas.client && (
+								{data.client && (
 									<div className="flex-1">
 										<h2 className="text-gray-400 font-light text-sm mb-[.8em]">Client</h2>
-										<p className="text-sm">{datas.client}</p>
+										<p className="text-sm">{data.client}</p>
 									</div>
 								)}
 
-								{datas.description && (
+								{data.description && (
 									<div className="flex-1 desc">
 										<h2 className="text-gray-400 font-light text-sm mb-[.8em]">Description</h2>
 										<p className="text-sm">
-											{datas.description}
+											{data.description}
 										</p>
 									</div>
 								)}
 
-								{datas.categories && (
+								{data.categories && (
 									<div className="services">
 										<h2 className="text-gray-400 font-light text-sm mb-[.8em]">Services</h2>
 										<ul className="text-sm">
-											{datas.categories.map((category: any) => (
+											{data.categories.map((category: any) => (
 												<li key={category._id}>{category.title}</li>
 											))}
 										</ul>
@@ -66,11 +125,11 @@ const Project = ({ datas }: Props) => {
 							</div>
 						</section>
 
-						{datas.body && (
+						{data.body && (
 							<PortableText
-								dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+								dataset={process.env.NEXT_PUBLIC_SANITY_DATAET}
 								projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-								content={datas.body}
+								content={data.body}
 								serializers={{
 									h1: (props: any) => (<h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">{props.children}</h1>),
 									h2: (props: any) => (<h2 className="text-2xl md:text-3xl lg:text-4xl font-bold">{props.children}</h2>),
@@ -87,36 +146,4 @@ const Project = ({ datas }: Props) => {
 	)
 }
 
-export default Project
-
-export const getStaticPaths = async () => {
-	const datas = await client.fetch(allProjects)
-
-	const path = datas.map((data: any) => ({
-		params: { slug: data.slug.current },
-	}))
-
-	return {
-		paths: path,
-		fallback: 'blocking',
-	}
-}
-
-export const getStaticProps = async ({ params }: any) => {
-	const datas = await client.fetch(projectsPostsQuery, { slug: params?.slug })
-
-	if (!datas) {
-		return {
-			notFound: true
-		}
-	}
-
-	return {
-		props: {
-			datas
-		},
-
-		revalidate: 60 // after 60 seconds, it will be regenerated
-	}
-}
 
