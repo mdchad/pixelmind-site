c8a9730dfb7ee17254522cdb7bd8815005b470dc
mdchad <irsyad.muhd@gmail.com>
2023-03-27 23:23:51 +0800

    testing og

--- src/components/head-meta.tsx
+++ src/components/head-meta.tsx
@@ -2,12 +2,12 @@ import { SEO } from '@root/typings';
 import Head from 'next/head';
 import React from 'react'
 
-function HeadMeta({ title, description, keywords, image }: SEO) {
+function HeadMeta({ title= "", description= "", keywords= "", image= "" }: SEO) {
 	const defaultTitle = "Pixelmind Studio | Building Bridges Between Business and Technology";
 	const defaultDescription = "At Pixelmind Studio, we specialize in bridging the gap between business and technology. Our team delivers custom software solutions, web design, and digital marketing services that help businesses grow.";
 	const defaultKey = "custom technology solutions, web design, software development, digital marketing, business growth, technology consulting, innovation, digital transformation, project management, user experience";
 	const ogUrl = "/api/og-image";
-	let ogImage = '';
+	let ogImage = "/og2.png";
 
 	if (!image) {
 		const url = typeof window !== 'undefined' ? window.location.origin : '';
@@ -20,13 +20,20 @@ function HeadMeta({ title, description, keywords, image }: SEO) {
 			<meta name="description" content={description ?? defaultDescription} />
 			<meta name="keywords" content={keywords ?? defaultKey} />
 
+			{/*<meta property="og:url" content={`https://subscription-starter.vercel.app${router.asPath}`} />*/}
+			{/*<meta property="og:type" content="website" />*/}
+			{/*<meta property="og:site_name" content={meta.title} />*/}
+			{/*<meta property="og:description" content={meta.description} />*/}
+			{/*<meta property="og:title" content={meta.title} />*/}
+			{/*<meta property="og:image" content={meta.cardImage} />*/}
+
 			{/* OG image */}
-			<meta property="og:image" content={image ?? ogImage} />
 			<meta property="og:url" content="https://www.pixelmindstudio.co/" />
 			<meta property="og:type" content="website" />
 			<meta property="og:site_name" content={title ?? defaultTitle} />
-			<meta property="og:title" content={title ?? defaultTitle} />
 			<meta property="og:description" content={description ?? defaultDescription} />
+			<meta property="og:image" content={image ?? ogImage} />
+			<meta property="og:title" content={title ?? defaultTitle} />
 
 			{/* twitter OG Meta */}
 			<meta name="twitter:card" content="summary_large_image" />
