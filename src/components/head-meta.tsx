990df9ed58e2136abebec7340b1f79662a3151b4
Muhammad Irsyad <irsyad.muhd@gmail.com>
2023-03-28 21:44:40 +0800

    Bugfix/og image (#13)
    * testing og
    
    * remove conditional
    
    * static image
    
    * add url
    
    * remove slash
    
    * fix order
    
    * hardcode
    
    * move to app
    
    * change url
    
    * fix url
    
    * try with conditional
    
    * fix image link
    
    * fix url

--- src/components/head-meta.tsx
+++ src/components/head-meta.tsx
@@ -7,22 +7,27 @@ function HeadMeta({ title, description, keywords, image }: SEO) {
 	const defaultDescription = "At Pixelmind Studio, we specialize in bridging the gap between business and technology. Our team delivers custom software solutions, web design, and digital marketing services that help businesses grow.";
 	const defaultKey = "custom technology solutions, web design, software development, digital marketing, business growth, technology consulting, innovation, digital transformation, project management, user experience";
 	const ogUrl = "/api/og-image";
-	let ogImage = '';
+	let ogImage = "";
 
 	if (!image) {
-		const url = typeof window !== 'undefined' ? window.location.origin : '';
-		ogImage = url + ogUrl;
+		if (process.env.NODE_ENV === 'production') {
+			ogImage = process.env.NEXT_PUBLIC_URL + ogUrl;
+		} else {
+			ogImage = 'http://localhost:3000/' + ogUrl;
+		}
 	}
 
 	return (
 		<Head>
 			<title>{title ?? defaultTitle}</title>
+			<meta name="robots" content="follow, index" />
+			<link href="/favicon.ico" rel="shortcut icon" />
 			<meta name="description" content={description ?? defaultDescription} />
 			<meta name="keywords" content={keywords ?? defaultKey} />
 
 			{/* OG image */}
 			<meta property="og:image" content={image ?? ogImage} />
-			<meta property="og:url" content="https://www.pixelmindstudio.co/" />
+			<meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
 			<meta property="og:type" content="website" />
 			<meta property="og:site_name" content={title ?? defaultTitle} />
 			<meta property="og:title" content={title ?? defaultTitle} />
@@ -36,9 +41,6 @@ function HeadMeta({ title, description, keywords, image }: SEO) {
 			<meta name="twitter:title" content={title ?? defaultTitle} />
 			<meta name="twitter:description" content={description ?? defaultDescription} />
 			<meta name="twitter:image" content={image ?? ogImage} />
-
-			<meta name="robots" content="follow, index" />
-			<link href="/favicon.ico" rel="shortcut icon" />
 		</Head>
 	)
 }
