c7980dceeb62c35fcfb3509439b819643533b753
mdchad <irsyad.muhd@gmail.com>
2023-03-28 21:08:07 +0800

    fix url

--- src/components/head-meta.tsx
+++ src/components/head-meta.tsx
@@ -7,38 +7,27 @@ function HeadMeta({ title, description, keywords, image }: SEO) {
 	const defaultDescription = "At Pixelmind Studio, we specialize in bridging the gap between business and technology. Our team delivers custom software solutions, web design, and digital marketing services that help businesses grow.";
 	const defaultKey = "custom technology solutions, web design, software development, digital marketing, business growth, technology consulting, innovation, digital transformation, project management, user experience";
 	const ogUrl = "/api/og-image";
-	let ogImage = "/og2.png";
+	let ogImage = "";
 
 	if (!image) {
-		const url = typeof window !== 'undefined' ? window.location.origin : '';
-		ogImage = 'https://pixelmind-site-git-bugfix-og-image-mdchad.vercel.app' + ogUrl;
+		if (process.env.NODE_ENV === 'production') {
+			ogImage = process.env.NEXT_PUBLIC_URL + ogUrl;
+		} else {
+			ogImage = 'http://localhost:3000/' + ogUrl;
+		}
 	}
 
 	return (
 		<Head>
 			<title>{title ?? defaultTitle}</title>
-			{/*<meta property="og:image" content={ogImage} />*/}
-			{/*<meta property="og:url" content="https://pixelmind-site-git-bugfix-og-image-mdchad.vercel.app" />*/}
-			{/*<meta property="og:type" content="website" />*/}
-			{/*<meta property="og:site_name" content={defaultTitle} />*/}
-			{/*<meta property="og:description" content={defaultDescription} />*/}
-			{/*<meta property="og:title" content={defaultTitle} />*/}
-
 			<meta name="robots" content="follow, index" />
 			<link href="/favicon.ico" rel="shortcut icon" />
 			<meta name="description" content={description ?? defaultDescription} />
 			<meta name="keywords" content={keywords ?? defaultKey} />
 
-			{/*<meta property="og:url" content={`https://pixelmind-site-git-bugfix-og-image-mdchad.vercel.app/`} />*/}
-			{/*<meta property="og:type" content="website" />*/}
-			{/*<meta property="og:site_name" content={`Pixelmind Studio | Building Bridges Between Business and Technology`} />*/}
-			{/*<meta property="og:description" content={`At Pixelmind Studio, we specialize in bridging the gap between business and technology. Our team delivers custom software solutions, web design, and digital marketing services that help businesses grow.`} />*/}
-			{/*<meta property="og:title" content={`Pixelmind Studio | Building Bridges Between Business and Technology`} />*/}
-			{/*<meta property="og:image" content={`https://pixelmind-site-git-bugfix-og-image-mdchad.vercel.app/og2.png`} />*/}
-
 			{/* OG image */}
 			<meta property="og:image" content={image ?? ogImage} />
-			<meta property="og:url" content="https://www.pixelmindstudio.co/" />
+			<meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
 			<meta property="og:type" content="website" />
 			<meta property="og:site_name" content={title ?? defaultTitle} />
 			<meta property="og:title" content={title ?? defaultTitle} />
